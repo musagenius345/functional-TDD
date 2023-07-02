5f6ec75dbcd92e6dcfba83d3b224001a85ec7251
musagenius345 <musabadru@gmail.com>
2023-07-02 05:03:05 +0300

    algo

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,15 +1,21 @@
-import { newArray } from './arrayGenerator.js'
-
 export const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
-      if(arr[j] > arr[j + 1]){
-        [arr[j], arr[j + 1]] = [arr[j +1 ], arr[j]]
+  if (arr.length === 0) {
+    return []
+  } else if(arr.length === 1){
+    return arr
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        }
       }
     }
+    return arr
   }
-  return arr
 }
+
+
 
 
 
--- js/slice.js
+++
@@ -1,6 +0,0 @@
-let sentence = 'Hello New World'
-
-let sliced = sentence.slice(-1)
-let sliced = sentence.slice(-1)
-console.log(sliced)
-console.log(sentence)
