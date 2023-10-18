109d798074e1be259a7636276dfbbbaf3f02bfa4
musagenius345 <musabadru@gmail.com>
2023-10-18 22:15:43 +0300

    map alt solution to 0003

--- leetcode/0003.lengthOfLongestSubstring.ts
+++ leetcode/0003.lengthOfLongestSubstring.ts
@@ -14,3 +14,23 @@ export function lengthOfLongestSubstring(s: string): number {
 
   return maxLength;
 }
+
+
+export function lengthOfLongestSubstringMap(s: string): number {
+    if (!s) return 0;
+
+    let maxLength = 0;
+    let left = 0;
+    let charIndexMap = new Map<string, number>();
+
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        if (charIndexMap.has(char) && charIndexMap.get(char)! >= left) {
+            left = charIndexMap.get(char)! + 1;
+        }
+        charIndexMap.set(char, right);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
--- leetcode/index.test.js
+++ leetcode/index.test.js
@@ -1,5 +1,4 @@
-
-import { addTwoNumbers, ListNode, addTwoNumbersRecursive, lengthOfLongestSubstring} from './index.js';
+import { addTwoNumbers, ListNode, addTwoNumbersRecursive, lengthOfLongestSubstringMap, lengthOfLongestSubstring } from './index.js';
 
 describe('addTwoNumbers', () => {
   it('adds two numbers correctly', () => {
@@ -60,41 +59,43 @@ describe('addTwoNumbersRecursive', () => {
   });
 });
 
+function lengthOfLongestSubstringTest(func, name) {
+  describe(`${name}`, () => {
+    // Test cases for empty string
+    test('Empty string should return 0', () => {
+      expect(func('')).toBe(0);
+    });
 
-describe('lengthOfLongestSubstring function', () => {
-  // Test cases for empty string
-  test('Empty string should return 0', () => {
-    expect(lengthOfLongestSubstring('')).toBe(0);
+    // Test cases for strings with unique characters
+    test('String with unique characters should return its length', () => {
+      expect(func('abcde')).toBe(5);
+      expect(func('12345')).toBe(5);
+    });
+
+    // Test cases for strings with repeating characters
+    test('String with repeating characters should return length of longest substring without repeating characters', () => {
+      expect(func('pwwkew')).toBe(3); // Longest substring without repeating characters: 'wke'
+      expect(func('bbbbb')).toBe(1); // Longest substring without repeating characters: 'b'
+      expect(func('abcabcbb')).toBe(3); // Longest substring without repeating characters: 'abc'
+    });
+
+    // Additional test cases with various characters and lengths
+    test('String with mix of characters should return correct result', () => {
+      expect(func('a b c d e')).toBe(3); // Longest substring without repeating characters: 'a '
+      expect(func('112233 44 55')).toBe(3); // Longest substring without repeating characters: ' 44'
+    });
+
+    // Edge case tests
+    test('String with maximum length should execute within time limits', () => {
+      const maxLengthString = 'a'.repeat(5 * 10 ** 4);
+      expect(func(maxLengthString)).toBe(1); // All characters are the same, so the length is 1
+    });
+
+    test('String with single character should return 1', () => {
+      expect(func('a')).toBe(1);
+    });
   });
+}
 
-  // Test cases for strings with unique characters
-  test('String with unique characters should return its length', () => {
-    expect(lengthOfLongestSubstring('abcde')).toBe(5);
-    expect(lengthOfLongestSubstring('12345')).toBe(5);
-  });
-
-  // Test cases for strings with repeating characters
-  test('String with repeating characters should return length of longest substring without repeating characters', () => {
-    expect(lengthOfLongestSubstring('pwwkew')).toBe(3); // Longest substring without repeating characters: 'wke'
-    expect(lengthOfLongestSubstring('bbbbb')).toBe(1); // Longest substring without repeating characters: 'b'
-    expect(lengthOfLongestSubstring('abcabcbb')).toBe(3); // Longest substring without repeating characters: 'abc'
-  });
-
-  // Additional test cases with various characters and lengths
-  test('String with mix of characters should return correct result', () => {
-    expect(lengthOfLongestSubstring('a b c d e')).toBe(3); // Longest substring without repeating characters: 'a '
-    expect(lengthOfLongestSubstring('112233 44 55')).toBe(3); // Longest substring without repeating characters: ' 44'
-  });
-
-  // Edge case tests
-  test('String with maximum length should execute within time limits', () => {
-    const maxLengthString = 'a'.repeat(5 * 10**4);
-    expect(lengthOfLongestSubstring(maxLengthString)).toBe(1); // All characters are the same, so the length is 1
-  });
-
-  test('String with single character should return 1', () => {
-    expect(lengthOfLongestSubstring('a')).toBe(1);
-  });
-});
-
-
+lengthOfLongestSubstringTest(lengthOfLongestSubstringMap, 'lengthOfLongestSubstringMap')
+lengthOfLongestSubstringTest(lengthOfLongestSubstring, 'lengthOfLongestSubstring')
