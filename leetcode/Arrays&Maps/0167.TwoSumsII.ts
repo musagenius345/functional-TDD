eca6f415e23804404c1967fc0a64fbc3f59679eb
musagenius345 <musabadru@gmail.com>
2023-10-21 22:11:33 +0300

    0167 Two Sums for sorted arrays

---
+++ leetcode/Arrays&Maps/0167.TwoSumsII.ts
@@ -0,0 +1,18 @@
+export function twoSumMid(numbers: number[], target: number): number[] {
+  let right = numbers.length - 1
+  let left = 0
+
+  while(left < right){
+    const sum = numbers[right] + numbers[left]
+    if(sum === target){
+      return [left + 1, right + 1]
+    } else if (sum < target){
+      left++
+    } else {
+      right--
+    }
+  }
+
+
+  return []
+}
