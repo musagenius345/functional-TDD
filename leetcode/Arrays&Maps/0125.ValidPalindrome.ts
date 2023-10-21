221ec1dae3e6c4dba3e91cd9dd2b9dd5d1b70617
musagenius345 <musabadru@gmail.com>
2023-10-21 17:50:32 +0300

    0125 Valid Palindrome

---
+++ leetcode/Arrays&Maps/0125.ValidPalindrome.ts
@@ -0,0 +1,44 @@
+export function isPalindrome(s: string) {
+  const clean = s.toLowerCase().replace(/[\W_]/g, '')
+
+  const reverseWord = clean.split('').reverse().join('')
+
+  return clean === reverseWord
+  
+}
+
+
+
+export function isPalindromeTwo(s: string): boolean {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+function isAlphanumeric(char: string): boolean {
+    return /^[0-9a-zA-Z]$/.test(char);
+}
+
+// const strOne = 'A man, a plan, a canal: Panama'
+const strTwo = 'a_b'
+// console.log(isPalindrome(strOne));
+console.log(isPalindrome(strTwo));
+
