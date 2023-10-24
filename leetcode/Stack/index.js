408f891642ba042da20436297b58c62a65c4db5e
musagenius345 <musabadru@gmail.com>
2023-10-24 12:40:37 +0300

    150 tests and imports

--- leetcode/Stack/index.js
+++ leetcode/Stack/index.js
@@ -1,2 +1,3 @@
 export { isValid }  from '0020.ValidParentheses'
 export { MinStack }  from '0155.MinStack'
+export { evalRPN }  from '0150.ReversePolishNotation'
--- leetcode/Stack/index.test.js
+++ leetcode/Stack/index.test.js
@@ -1,4 +1,41 @@
-import { isValid, MinStack } from 'index.js'
+import { isValid, MinStack, evalRPN } from 'index.js'
+
+// LEARN a × (b + c) is written a b c + ×
+describe('Evaluate Reverse Polish Notation', () => {
+    it('should evaluate basic arithmetic operations correctly', () => {
+        expect(evalRPN(['2', '1', '+'])).toEqual(3); // 2 + 1 = 3
+        expect(evalRPN(['4', '10', '5', '/', '+'])).toEqual(6); // 4 + (10 / 5) = 6
+        expect(evalRPN(['12', '9', '6', '3', '/', '-', '*'])).toEqual(84); // 12 * ((9 - 6) / 3) = 12
+        expect(evalRPN(['18', '2', '/'])).toEqual(9); // 18 / 2 = 9
+    });
+
+    it('should handle negative numbers and subtraction correctly', () => {
+        expect(evalRPN(['-1', '1', '-'])).toEqual(-2); // -1 - 1 = -2
+        expect(evalRPN(['-1', '-1', '-'])).toEqual(0); // -1 - (-1) = 0
+    });
+
+    it('should handle multiplication and division correctly', () => {
+        expect(evalRPN(['2', '3', '*'])).toEqual(6); // 2 * 3 = 6
+        expect(evalRPN(['8', '4', '/'])).toEqual(2); // 8 / 4 = 2
+        expect(evalRPN(['20', '5', '*'])).toEqual(100); // 20 * 5 = 100
+        expect(evalRPN(['100', '10', '/'])).toEqual(10); // 100 / 10 = 10
+    });
+
+    it('should handle complex expressions correctly', () => {
+        expect(evalRPN(['4', '110', '10', '/', '+', '2', '*'])).toEqual(30); // (4 + (110 / 10)) * 2 = 30
+        expect(evalRPN(['3', '4', '*', '2', '5', '*', '+'])).toEqual(22); // (3 * 4) + (2 * 5) = 23
+    });
+
+    it('should handle edge cases and single-number expressions', () => {
+        expect(evalRPN(['0', '0', '/'])).toEqual(0); // Division by zero edge case
+        expect(evalRPN(['3'])).toEqual(3); // Single-number expression
+    });
+
+    it('should return NaN for invalid RPN expressions', () => {
+        expect(isNaN(evalRPN(['+', '2', '3']))).toBe(true); // Invalid RPN expression
+    });
+});
+
 
 
 describe("Valid Parentheses Order", () => {
