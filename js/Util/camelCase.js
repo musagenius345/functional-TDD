296145fe1b688d71dc5ba7485f42f390edcec4e3
musagenius345 <musabadru@gmail.com>
2023-11-27 17:26:15 +0300

    handled kebab

--- js/Util/camelCase.js
+++ js/Util/camelCase.js
@@ -1,8 +1,4 @@
-export const camelCase = (str) => {
-  // const delimiters = new Set('-', ' ')
-return str.toLowerCase()
-    .split(' ')
-    .map( (el, i) => i > 0 ? el.slice(0, 1).toUpperCase() + el.slice(1) : el).join('')
-}
-// const result = camelCase('Hello japan MiyagI')
-// console.log(result)
+
+export const camelCase = (str) => str.toLowerCase().split(/[\s-]/).map((el, i) => i > 0 ? el.slice(0, 1).toUpperCase() + el.slice(1) : el).join('');
+const result = camelCase('Hello japan-MiyagI')
+console.log(result)
